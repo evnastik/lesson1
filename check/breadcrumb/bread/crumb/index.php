39f5b6579d40d07a29204e619c6028923528c848
evnastik <evnastik@gmail.com>
2021-03-30 19:39:53 +0500

    Многоуровневый раздел

---
+++ check/breadcrumb/bread/crumb/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once ($_SERVER["DOCUMENT_ROOT"] . '/bitrix/header.php');
+
+/** @global CMain $APPLICATION */
+$APPLICATION->SetTitle("Crumb");
+
+echo 'Crumb';
+
+require_once ($_SERVER["DOCUMENT_ROOT"] . '/bitrix/footer.php');
---
+++ check/breadcrumb/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once ($_SERVER["DOCUMENT_ROOT"]. '/bitrix/header.php');
+
+/** @global CMain $APPLICATION */
+$APPLICATION->SetTitle('Breadcrumb');
+
+echo 'Breadcrumb test';
+
+require_once ($_SERVER["DOCUMENT_ROOT"]. '/bitrix/footer.php');
+
